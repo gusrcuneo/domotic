d313e14bc037fbb380620c7ccb413610e950eb08
Eloy Gomez <eloy@indeos.es>
2012-08-11 16:05:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Domotic::Application.config.secret_token = 'd1faacb48348dd2fd573789ac46e49a6c70d1ab0562387dfbc049729e540a7c2d82d6878db9b5bad863f0c317ef4c24f349f902e69cc4f931504fc9a63dadfad'
